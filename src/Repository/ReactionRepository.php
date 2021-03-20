ec335f76dfc84de056646ac76acfcb8feb49dd04
Abderrahime Sanadi <abderrahime.sanadi@gmail.com>
2021-03-20 05:37:58 -0400

    generate CRUD endpoints for 4 classes

---
+++ src/Repository/ReactionRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Reaction;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Reaction|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Reaction|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Reaction[]    findAll()
+ * @method Reaction[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ReactionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Reaction::class);
+    }
+
+}
